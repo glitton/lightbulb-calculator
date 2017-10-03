4d7af0e3828e627419200c5e23fd2d4873112896
Generosa Litton <generosa.litton@yahoo.com>
2017-10-02 19:22:09 -0700

    added first angular pieces

--- js/calculator.js
+++ js/calculator.js
@@ -1,19 +1,28 @@
 /* JavaScript Document */
+// Contains the Angular app
 
 (function(){
+  var app = angular.module('myCalculator', []);
 
-
-
-
-
-
-
-
-
-
-
+  app.controller('calcController', ['$scope', function($scope){
 
     
+
+
+  }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
 })();
 
 
