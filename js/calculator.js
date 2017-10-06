2ff038bf9efdd5eb716c40f471a72bc25eb84a0e
Generosa Litton <generosa.litton@yahoo.com>
2017-10-05 17:57:16 -0700

    fixed scope issue

--- js/calculator.js
+++ js/calculator.js
@@ -5,9 +5,15 @@
   var app = angular.module('myCalculator', []);
 
   app.controller('calcController', ['$scope', function($scope){
-
-    
-
+    // declare variables for select menu
+    // lumens options
+    $scope.lumen_options = [376, 600, 900, 1125, 1600];
+    // set current lumens when the page loads
+    $scope.current_lumens = 600;
+    // set current cost, hours, total days when the page loads
+    $scope.current_cost = 13;
+    $scope.current_hours = 4;
+    $scope.total_days = 365; 
 
   }]);
 
